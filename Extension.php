0f4cc8d528329658e4f1b76732ab04c65fc99ad9
Miguel Angel <miguela.vaqrod@gmail.com>
2015-03-25 20:13:02 +0100

    Create Extension.php

---
+++ Extension.php
@@ -0,0 +1,13 @@
+<?php
+namespace Bolt\Extension\miguelavaqrod\diy;
+class Extension extends \Bolt\BaseExtension
+{
+    public function getName()
+    {
+        return "DIY";
+    }
+    public function initialize()
+    {
+
+    }
+}
